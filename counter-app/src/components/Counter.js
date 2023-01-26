ff6793537ef8cda332687aec543536726b0a7697
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-26 19:26:05 +0300

    added action functions and called related component

--- counter-app/src/components/Counter.js
+++ counter-app/src/components/Counter.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../redux/counter/counterSlice";
 
 function Counter() {
   const countValue = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
-  return <div>{countValue}</div>;
+  return (
+    <div>
+      <h3>{countValue}</h3>
+
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
 }
 
 export default Counter;

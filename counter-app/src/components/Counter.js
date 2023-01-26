3c5f20b03d8b4426895147d77e542d31c224c809
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-26 19:42:43 +0300

    usage action.payload

--- counter-app/src/components/Counter.js
+++ counter-app/src/components/Counter.js
@@ -1,17 +1,21 @@
-import React from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "../redux/counter/counterSlice";
+import { increment, decrement, incrementByAmount } from "../redux/counter/counterSlice";
 
 function Counter() {
   const countValue = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
-
+  const [amount, setAmount] = useState(0);
   return (
     <div>
       <h3>{countValue}</h3>
 
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <br />
+      <br />
+      <input type="number" onChange={(e) => setAmount(e.target.value)} value={amount} />
+      <button onClick={() => dispatch(incrementByAmount(amount))}>Increment by Amount</button>
     </div>
   );
 }

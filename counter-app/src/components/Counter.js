34981922050a6f60b8ed07393c2c1cf23d995401
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-26 19:11:50 +0300

    added a component and state data

---
+++ counter-app/src/components/Counter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+function Counter() {
+  const countValue = useSelector((state) => state.counter.value);
+
+  return <div>{countValue}</div>;
+}
+
+export default Counter;

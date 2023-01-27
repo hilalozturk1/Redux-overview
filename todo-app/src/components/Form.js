3664a864fe9eae3d1d56b61cb15e96a57fb9ccb0
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-27 12:58:04 +0300

    used the action in this component

--- todo-app/src/components/Form.js
+++ todo-app/src/components/Form.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { addTodo } from "../redux/todos/todosSlice";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
 
 function Form() {
+  const [title, setTitle] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(addTodo({ id: nanoid(), title, completed: false }));
+
+    setTitle("");
+  };
+
   return (
-    <form action="">
-      <input className="new-todo" placeholder="What needs to be done?" autoFocus />
+    <form onSubmit={handleSubmit}>
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        value={title}
+        autoFocus
+      />
     </form>
   );
 }

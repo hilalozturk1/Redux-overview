dfc496303a91d9dc3d9dc224e0c4111a3c518fba
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-31 18:51:27 +0300

    usage create entity adaptor

---
+++ contact-app/src/redux/contactSlice.js
@@ -0,0 +1,17 @@
+import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+
+export const contactAdaptor = createEntityAdapter(); //
+
+export const contactSelectors = contactAdaptor.getSelectors((state) => state.contacts);
+
+const contactSlice = createSlice({
+  name: "contacts",
+  initialState: contactAdaptor.getInitialState(),
+  reducers: {
+    addContact: contactAdaptor.addOne,
+    addContacts: contactAdaptor.addMany,
+  },
+});
+
+export const { addContact, addContacts } = contactSlice.actions;
+export default contactSlice.reducer;
--- contact-app/src/redux/store.js
+++ contact-app/src/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import contactSlice from "./contactSlice";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    contacts: contactSlice,
+  },
 });

bebeb03c7876ebbbf514f87a717a90dd18e470ba
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-31 16:23:04 +0300

    defined update count state

--- spend-bill-gates-money/src/redux/products/productsSlice.js
+++ spend-bill-gates-money/src/redux/products/productsSlice.js
@@ -1,15 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 import products from "../../products.json";
 
-const data = products.products;
+const data = products;
 
 export const productsSlice = createSlice({
   name: "products",
   initialState: {
-    items: data,
+    items: data.products,
     budget: 100000000000,
   },
-  reducers: {},
+  reducers: {
+    updateCount: (state, action) => {
+      const { count, id } = action.payload;
+
+      const product = state.items.find((product) => id === product.id);
+      product.count = count;
+
+      let price = 0;
+      state.items.map((item) => (price += Number(item.count) * Number(item.productPrice)));
+
+      state.budget = 100000000000 - Number(price);
+    },
+  },
 });
 
+export const { updateCount } = productsSlice.actions;
 export default productsSlice.reducer;

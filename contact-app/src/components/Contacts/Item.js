eda632d4bde49f577dfbd7548a69f14ea02a9187
hilalozturk1 <hlloztrk68@gmail.com>
2023-02-01 12:41:17 +0300

    added edit link

--- contact-app/src/components/Contacts/Item.js
+++ contact-app/src/components/Contacts/Item.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { deleteContact } from "../../redux/contactSlice";
+import { Link } from "react-router-dom";
 
 function Item({ item }) {
   const dispatch = useDispatch();
@@ -13,6 +14,7 @@ function Item({ item }) {
   return (
     <div>
       <span>{item.name}</span> <span>{item.phone_number}</span>
+      <Link to={`/edit/${item.id}`}>Edit</Link>
       <button onClick={() => handleDelete(item.id)}>Delete</button>
     </div>
   );

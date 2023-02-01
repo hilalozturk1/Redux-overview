b20d123e15489d077f2e8326bcde9e207da0fc83
hilalozturk1 <hlloztrk68@gmail.com>
2023-02-01 12:39:03 +0300

    shown total

--- contact-app/src/components/Contacts/index.js
+++ contact-app/src/components/Contacts/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Form from "./Form";
 import List from "./List";
+import { useSelector } from "react-redux";
+import { contactSelectors } from "../../redux/contactSlice";
 
 function Contacts() {
+  const total = useSelector(contactSelectors.selectTotal);
   return (
     <div>
-      <h1>Contacts</h1>
+      <h1>Contacts {total}</h1>
       <Form />
       <List />
     </div>

ac5057f493b1d3888378f2ee665d7ae504c464f3
hilalozturk1 <hlloztrk68@gmail.com>
2023-02-01 12:41:50 +0300

    added remove all func

--- contact-app/src/components/Contacts/List.js
+++ contact-app/src/components/Contacts/List.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { contactSelectors } from "../../redux/contactSlice";
-import { useSelector } from "react-redux";
+import { contactSelectors, removeAllContact } from "../../redux/contactSlice";
+import { useSelector, useDispatch } from "react-redux";
 import Item from "./Item";
 
 function List() {
   const contacts = useSelector(contactSelectors.selectAll);
-
+  const total = useSelector(contactSelectors.selectTotal);
+  const dispatch = useDispatch();
+  const handleDeleteAll = () => {
+    if (window.confirm("are you sure")) {
+      dispatch(removeAllContact());
+    }
+  };
   return (
     <div>
+      {total > 0 && <div onClick={handleDeleteAll}>Remove All</div>}
       {contacts.map((item) => (
         <Item key={item.id} item={item} />
       ))}

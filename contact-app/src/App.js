a20ee9c967d0e22606d8c461cf0cd0eb6b73598a
hilalozturk1 <hlloztrk68@gmail.com>
2023-02-01 12:37:12 +0300

    routing

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import Contacts from "./components/Contacts";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Edit from "./components/Contacts/Edit";
 
 function App() {
   return (
     <div className="App">
-      <Contacts />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Contacts />}></Route>
+          <Route path="/edit/:id" element={<Edit />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }

2c1e02a61552aaf36b2321684aad6f45145ead8e
hilalozturk1 <hlloztrk68@gmail.com>
2023-02-12 19:17:46 +0300

    data

---
+++ memory-game/src/data.js
@@ -0,0 +1,115 @@
+const cardsArray = [
+  {
+    id: "1",
+    name: "shell",
+    img: "img/blueshell.png",
+    isMatched: false
+  },
+  {
+    id: "2",
+    name: "star",
+    img: "img/star.png",
+    isMatched: false
+  },
+  {
+    id: "3",
+    name: "bobomb",
+    img: "img/bobomb.png",
+    isMatched: false
+  },
+  {
+    id: "4",
+    name: "mario",
+    img: "img/mario.png",
+    isMatched: false,
+    isOpen : false
+  },
+  {
+    id: "5",
+    name: "luigi",
+    img: "img/luigi.png",
+    isMatched: false
+  },
+  {
+    id: "6",
+    name: "peach",
+    img: "img/peach.png",
+    isMatched: false
+  },
+  {
+    id: "7",
+    name: "1up",
+    img: "img/1up.png",
+    isMatched: false
+  },
+  {
+    id: "8",
+    name: "mushroom",
+    img: "img/mushroom.png",
+    isMatched: false
+  },
+  {
+    id: "9",
+    name: "thwomp",
+    img: "img/thwomp.png",
+    isMatched: false
+  },
+  {
+    id: "10",
+    name: "shell",
+    img: "img/blueshell.png",
+    isMatched: false
+  },
+  {
+    id: "11",
+    name: "star",
+    img: "img/star.png",
+    isMatched: false
+  },
+  {
+    id: "12",
+    name: "bobomb",
+    img: "img/bobomb.png",
+    isMatched: false
+  },
+  {
+    id: "13",
+    name: "mario",
+    img: "img/mario.png",
+    isMatched: false
+  },
+  {
+    id: "14",
+    name: "luigi",
+    img: "img/luigi.png",
+    isMatched: false
+  },
+  {
+    id: "15",
+    name: "peach",
+    img: "img/peach.png",
+    isMatched: false
+  },
+  {
+    id: "16",
+    name: "1up",
+    img: "img/1up.png",
+    isMatched: false
+  },
+  {
+    id: "17",
+    name: "mushroom",
+    img: "img/mushroom.png",
+    isMatched: false
+  },
+  {
+    id: "18",
+    name: "thwomp",
+    img: "img/thwomp.png",
+    isMatched: false
+  },
+];
+
+module.exports = {
+  cardsArray,
+};
